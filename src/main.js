d3181caaca60842665197743cd2d498a4e809d6a
GrisGGG <astropoetscapicua@gmail.com>
2022-07-16 20:45:17 -0500

    6. Modificando archivo js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-///movie/550?api_key=c572d58b3d3198d619b592b7f40321cf
-
 async function getTrendingMovies(){
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json()
