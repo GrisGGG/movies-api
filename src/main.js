9914a3063195224798167c8e9ec23fe3e1d815b0
GrisGGG <astropoetscapicua@gmail.com>
2022-07-17 01:40:38 -0500

    7. Añadiendo la segunda petición

--- src/main.js
+++ src/main.js
@@ -19,9 +19,31 @@ async function getTrendingMovies(){
         movieContainer.appendChild(movieImg);
         trendingPreviewContainer.appendChild(movieContainer)
     });
-
-
-
 }
+
+async function getCategoriesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json()
+    const categories = data.genres;
+    console.log({data, categories})
+
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container')
+        
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.setAttribute('id', 'id' + category.id)
+        categoryTitle.classList.add('category-title')
+        
+        
+        const categoryTitleText = document.createTextNode(category.name);
+        
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer)
+    });
+}
+getCategoriesPreview()
 
 getTrendingMovies()
