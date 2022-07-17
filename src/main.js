50a5ea3b71dd1d27c0bb96ffb7906af8ee1b0925
GrisGGG <astropoetscapicua@gmail.com>
2022-07-16 20:40:20 -0500

    5. Añadiendo el primer endpoint

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log("Hello");
+///movie/550?api_key=c572d58b3d3198d619b592b7f40321cf
+
+async function getTrendingMovies(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json()
+    const movies = data.results;
+    console.log({data, movies})
+
+    movies.forEach(movie => {
+        const movieContainer = document.createElement('div');
+        const trendingPreviewContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+        movieContainer.classList.add('movie-container')
+        
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute(
+            'src',
+            'https://image.tmdb.org/t/p/w300/' + movie.poster_path
+        )
+        movieContainer.appendChild(movieImg);
+        trendingPreviewContainer.appendChild(movieContainer)
+    });
+
+
+
+}
+
+getTrendingMovies()
