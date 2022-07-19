14925817321d74f406c5b7a41ed24c7441a72bd1
GrisGGG <astropoetscapicua@gmail.com>
2022-07-18 20:54:08 -0500

    10. Añadiendo archivo de nodes

---
+++ src/nodes.js
@@ -0,0 +1,39 @@
+//Mejoras posibles
+//const $ = (id) => document.querySelector(id);
+//const headerSection = $('#header');
+/**
+ * location.hash.startsWith('#trends')    ? trendsPage()       :
+location.hash.startsWith('#search=')   ? searchPage()       :
+location.hash.startsWith('#movie=')    ? movieDetailsPage() :
+location.hash.startsWith('#category=') ? categoriesPage()   :
+homePage()
+ */
+
+
+// Sections
+const headerSection = document.querySelector('#header');
+const trendingPreviewSection = document.querySelector('#trendingPreview');
+const categoriesPreviewSection = document.querySelector('#categoriesPreview');
+const genericSection = document.querySelector('#genericList');
+const movieDetailSection = document.querySelector('#movieDetail');
+
+// Lists & Containers
+const searchForm = document.querySelector('#searchForm');
+const trendingMoviesPreviewList = document.querySelector('.trendingPreview-movieList');
+const categoriesPreviewList = document.querySelector('.categoriesPreview-list');
+const movieDetailCategoriesList = document.querySelector('#movieDetail .categories-list');
+const relatedMoviesContainer = document.querySelector('.relatedMovies-scrollContainer');
+
+// Elements
+const headerTitle = document.querySelector('.header-title');
+const arrowBtn = document.querySelector('.header-arrow');
+const headerCategoryTitle = document.querySelector('.header-title--categoryView');
+
+const searchFormInput = document.querySelector('#searchForm input');
+const searchFormBtn = document.querySelector('#searchBtn');
+
+const trendingBtn = document.querySelector('.trendingPreview-btn');
+
+const movieDetailTitle = document.querySelector('.movieDetail-title');
+const movieDetailDescription = document.querySelector('.movieDetail-description');
+const movieDetailScore = document.querySelector('.movieDetail-score');
